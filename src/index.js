b93b938349d46464711ee8583d80369449f65f72
bilal-dallali <dallali-bilal@hotmail.fr>
2023-08-26 18:26:41 +0200

    update and delete notes

--- src/index.js
+++ src/index.js
@@ -9,15 +9,7 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/notes", async (req, res) => {
-    try {
-        const notes = await Note.find({})
-        res.send(notes)
-    } catch {
-        res.status(500).send(err)
-    }
-})
-
+// CRUD
 
 app.post("/notes", async (req, res) => {
     const note = await new Note(req.body)
@@ -28,6 +20,45 @@ app.post("/notes", async (req, res) => {
     }
     catch (err) {
         res.status(400).send(err)
+    }
+})
+
+app.get("/notes", async (req, res) => {
+    try {
+        const notes = await Note.find({})
+        res.send(notes)
+    } catch {
+        res.status(500).send(err)
+    }
+})
+
+app.patch("/notes/:id", async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id)
+
+        note.note = req.body.note
+
+        await note.save()
+
+        res.status(200).send(note)
+    } catch (err) {
+        res.status(404).send(err)
+    }
+
+})
+
+app.delete("/notes/:id", async (req, res) => {
+    
+    try {
+        const note = await Note.findByIdAndDelete(req.params.id)
+
+        if (!note) {
+            return res.status(404).send()
+        }
+
+        res.send("the note has been deleted")
+    } catch (err) {
+        res.status(500).send(err)
     }
 })
 
