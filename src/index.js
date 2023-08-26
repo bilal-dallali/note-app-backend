31190d6125dda4149e54698f0439451f9753ad3c
bilal-dallali <dallali-bilal@hotmail.fr>
2023-08-26 16:44:39 +0200

    post requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const fs = require("fs")
 
+require("./db/mongoose")
+
+const Note = require("./models/note")
+
 const app = express()
 
+app.use(express.json())
+
 app.get("/notes", (req, res) => {
-    fs.readFile()
+    const notes = Note.find({})
+})
+
+
+app.post("/notes", (req, res) => {
+    const note = new Note(req.body)
+
+    note.save()
+    .then(() => {
+        res.status(200).send(note)
+        console.log(note)
+    })
+    .catch((err) => {
+        res.status(500).send(err)
+        console.log(err)
+    })
 })
 
 app.listen(3000, () => {
